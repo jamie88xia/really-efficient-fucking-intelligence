e4970198e24df02de432e8bc4f63a20b0ae97def
BigXiazilla <bigxiazilla@BigXiazillas-MacBook-Pro.local>
2026-07-11 16:07:28 -0500

    fix: update to work with M hardware

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import calculatorInputReducer from '../features/calculatorInput/calculatorInputSlice';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     calculatorInput: calculatorInputReducer
   },
 });
